3a1d2348db4aa973bc83e60e776310fa741add1d
SharifAly <sharif.aly_@outlook.com>
2024-12-17 17:41:45 +0100

    fixed: send the jwt token back to backend

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -26,7 +26,7 @@ const Post = () => {
       return;
     } else {
       axios
-        .post("http://localhost:5000/blog/post", postData, {
+        .post("http://localhost:5000/blog/post", postData, { withCredentials: true }, {
           headers: {
             Authorization: "Bearer " + token,
           },
