eae7fe95f05a382848fc668df4beacf6ad3d146c
SharifAly <sharif.aly_@outlook.com>
2024-12-17 21:02:55 +0100

    hide: comment out the search bar in Navigation.js

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -95,14 +95,14 @@ const Navigation = () => {
             </NavLink>
           </div>
 
-          <div>
-            {/* Search Input */}
+          {/* <div>
+            Search Input
             <input
               type="text"
               placeholder="Search"
               className="border border-gray-300 rounded-md px-3 py-2 text-base focus:outline-none focus:border-blue-500"
             />
-          </div>
+          </div> */}
 
           <div>
             {isLoggedIn ? (
